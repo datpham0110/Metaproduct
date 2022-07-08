83efb6cbaf6c445281cde15cab3df6eace20ff60
htnhan-dev <htnhan.dev@gmail.com>
2022-07-08 14:15:10 +0700

    [Nhan commit] - fix scroll top on change router

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Home from "./pages/Home";
 import Details from "./pages/Details";
 import Blog from "./pages/Blog"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollTop from "./components/ScrollTop";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/detail" element={ <Details /> } />
-          <Route path="/blog" element={ <Blog /> } />
-        </Routes>
-      <Footer />
+      {/* Seting router change auto top */}
+      <ScrollTop>
+        <Header />
+          <Routes>
+            <Route path="/" element={ <Home />} />
+            <Route path="/detail" element={ <Details /> } />
+            <Route path="/blog" element={ <Blog /> } />
+          </Routes>
+        <Footer />
+      </ScrollTop>
     </BrowserRouter>
   );
 }
